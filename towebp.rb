a5746bc7995e8d74c1bac986174197c729538ad3
Carsten Witt <carsten.witt@gmail.com>
2017-04-28 13:47:39 +0200

    Typofix

--- towebp.rb
+++ towebp.rb
@@ -4,7 +4,7 @@ class Towebp < Formula
   url "https://github.com/germaniakg/towebp/archive/1.0.3.tar.gz"
   sha256 "ee22810fa88bba599d5aa09921fda18c72722265e6a2bf9db5f61d8a3f400317"
 
-  # Redundant sice version taken from archive link above.
+  # Redundant since version taken from archive link above.
   # version "1.0.3"
 
   bottle :unneeded
