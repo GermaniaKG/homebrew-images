61c4be83ebf7a60a0bda9a080fd284533586f3b2
Carsten Witt <carsten.witt@gmail.com>
2017-04-28 13:38:21 +0200

    Convert indentation to spaces

--- towebp.rb
+++ towebp.rb
@@ -1,15 +1,15 @@
 class Towebp < Formula
-	desc "A command line tool for simplified cwebp usage"
-	homepage "https://github.com/GermaniaKG/towebp"
-	url "https://api.github.com/repos/GermaniaKG/towebp/tarball/1.0.3"
-	sha256 "b31f993d6874c80e098a5c7ba78a83cda8e18a64141bd9d0cddbf20e2574e270"
-	version "1.0.3"
+    desc "A command line tool for simplified cwebp usage"
+    homepage "https://github.com/GermaniaKG/towebp"
+    url "https://api.github.com/repos/GermaniaKG/towebp/tarball/1.0.3"
+    sha256 "b31f993d6874c80e098a5c7ba78a83cda8e18a64141bd9d0cddbf20e2574e270"
+    version "1.0.3"
 
-	depends_on "webp"
+    depends_on "webp"
 
-	bottle :uneeded
+    bottle :uneeded
 
-	def install
-		bin.install "towebp"
-	end
+    def install
+        bin.install "towebp"
+    end
 end
