495abf96bf1921ca7334cac9b2f3df5e4c441bb7
Carsten Witt <carsten.witt@gmail.com>
2017-04-28 13:40:25 +0200

    Fix audit issues

--- towebp.rb
+++ towebp.rb
@@ -1,9 +1,11 @@
 class Towebp < Formula
     desc "A command line tool for simplified cwebp usage"
     homepage "https://github.com/GermaniaKG/towebp"
-    url "https://api.github.com/repos/GermaniaKG/towebp/tarball/1.0.3"
-    sha256 "b31f993d6874c80e098a5c7ba78a83cda8e18a64141bd9d0cddbf20e2574e270"
-    version "1.0.3"
+    url "https://github.com/germaniakg/towebp/archive/1.0.3.tar.gz"
+    sha256 "ee22810fa88bba599d5aa09921fda18c72722265e6a2bf9db5f61d8a3f400317"
+
+    # Redundant sice version taken from archive link above.
+    # version "1.0.3"
 
     depends_on "webp"
 
