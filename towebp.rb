5f7bb9d7fa0e90c6da15435cf1c77ab24a326cc3
Carsten Witt <carsten.witt@gmail.com>
2017-04-28 13:47:05 +0200

    Fixed strict audit issues

--- towebp.rb
+++ towebp.rb
@@ -1,17 +1,17 @@
 class Towebp < Formula
-    desc "A command line tool for simplified cwebp usage"
-    homepage "https://github.com/GermaniaKG/towebp"
-    url "https://github.com/germaniakg/towebp/archive/1.0.3.tar.gz"
-    sha256 "ee22810fa88bba599d5aa09921fda18c72722265e6a2bf9db5f61d8a3f400317"
+  desc "Command-line tool for simplified cwebp usage"
+  homepage "https://github.com/GermaniaKG/towebp"
+  url "https://github.com/germaniakg/towebp/archive/1.0.3.tar.gz"
+  sha256 "ee22810fa88bba599d5aa09921fda18c72722265e6a2bf9db5f61d8a3f400317"
 
-    # Redundant sice version taken from archive link above.
-    # version "1.0.3"
+  # Redundant sice version taken from archive link above.
+  # version "1.0.3"
 
-    depends_on "webp"
+  bottle :unneeded
 
-    bottle :unneeded
+  depends_on "webp"
 
-    def install
-        bin.install "towebp"
-    end
+  def install
+    bin.install "towebp"
+  end
 end
