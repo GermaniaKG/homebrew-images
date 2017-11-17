1d6fb6d3224ad19b1746c664f280f4478e293b4e
Carsten Witt <carsten.witt@gmail.com>
2017-11-17 13:28:21 +0100

    package update

--- towebp.rb
+++ towebp.rb
@@ -1,8 +1,8 @@
 class Towebp < Formula
   desc "Command-line tool for simplified cwebp usage"
   homepage "https://github.com/GermaniaKG/towebp"
-  url "https://github.com/germaniakg/towebp/archive/1.2.0.tar.gz"
-  sha256 "0044b34018cf4aef8f803f96e255f2f69af06bfd5a6c0a57ec15de0aef67380b"
+  url "https://github.com/germaniakg/towebp/archive/1.2.1.tar.gz"
+  sha256 "b8b7b570550b4a2d1d7b9cee2438bc66cd047798716874fa06b04b3b76fd6ecf"
 
   # Redundant since version taken from archive link above.
   # version "1.0.3"
