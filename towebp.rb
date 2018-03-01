d7d879a1b2c2aaf2ad52f7a0b974a0424d460c7c
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2018-03-01 13:05:43 +0100

    Fix checksum

--- towebp.rb
+++ towebp.rb
@@ -2,7 +2,7 @@ class Towebp < Formula
   desc "Command-line tool for simplified cwebp creation from existing files"
   homepage "https://github.com/GermaniaKG/towebp"
   url "https://github.com/germaniakg/towebp/archive/1.2.2.tar.gz"
-  sha256 "dd23ee2e06e2a03d15014abe4c3617019ef2cc843e905417cdcb1fc7c1c0cdbf"
+  sha256 "9b923ac657e10326cbc4dfbc05f7b87e0ff54dbe50ebaa928ae6dae3185642be"
 
   # Redundant since version taken from archive link above.
   # version "1.0.3"
