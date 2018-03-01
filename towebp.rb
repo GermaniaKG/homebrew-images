72a7848f13230a8e8628b99c0fc401f2f53e753e
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2018-03-01 12:53:37 +0100

    Change description

--- towebp.rb
+++ towebp.rb
@@ -1,5 +1,5 @@
 class Towebp < Formula
-  desc "Command-line tool for simplified cwebp usage"
+  desc "Command-line tool for simplified cwebp creation from existing files"
   homepage "https://github.com/GermaniaKG/towebp"
   url "https://github.com/germaniakg/towebp/archive/1.2.2.tar.gz"
   sha256 "dd23ee2e06e2a03d15014abe4c3617019ef2cc843e905417cdcb1fc7c1c0cdbf"
