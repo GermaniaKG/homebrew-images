32164f01c76a28e4353c014e09dcaa58e9dee666
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2018-05-18 09:28:28 +0200

    Update to new version

--- extract-title.rb
+++ extract-title.rb
@@ -1,8 +1,8 @@
 class ExtractTitle < Formula
   desc "Extract the first PDF page to a JPEG file"
   homepage "https://github.com/GermaniaKG/extract-title"
-  url "https://github.com/germaniakg/extract-title/archive/1.1.4.tar.gz"
-  sha256 "710434fd322c54e34ace4c1f1d379ca3c66595a202999d598b4bf8137a7bdef2"
+  url "https://github.com/germaniakg/extract-title/archive/1.1.5.tar.gz"
+  sha256 "23e3b389bbbb1459612feb66ba347fd2ad42c7d06e485fb05095c3648c8f9824"
 
   bottle :unneeded
 
